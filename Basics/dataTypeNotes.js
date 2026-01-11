0b0f039e7b085088613d1cf659d3698ac4fb7a66
Yogesh Mandor <yogeshmandor1@gmail.com>
2026-01-11 14:34:08 +0000

    DataType Notes

---
+++ Basics/dataTypeNotes.js
@@ -0,0 +1,33 @@
+/* There are two types of data
+ 1 Primitive 2. Non-Primitive
+  7 types of primitive data type 
+1. String 2. Number 3. Boolean 4. null 5. Undefined 6. Symbol 7.BigInt */
+
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const outSideTemp = null
+let useremail;
+
+
+console.log(useremail);
+
+
+// Reference (Non-Primitive)
+
+// There are three types
+// 1. Array , 2. Objects , 3. Functions
+
+
+const heros = ["Tiger" , "Allu" , "King"]
+
+let myObj = {
+    name : "Yogesh",
+    age : 21
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
